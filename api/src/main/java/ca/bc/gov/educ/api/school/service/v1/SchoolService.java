e5121a64e5eab7b3260efa28059d7c53dc186c6c
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2021-05-20 21:07:49 -0700

    remove thread switching...

--- api/src/main/java/ca/bc/gov/educ/api/school/service/v1/SchoolService.java
+++ api/src/main/java/ca/bc/gov/educ/api/school/service/v1/SchoolService.java
@@ -7,21 +7,17 @@ import ca.bc.gov.educ.api.school.model.v1.SchoolEntity;
 import ca.bc.gov.educ.api.school.repository.v1.SchoolRepository;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Function;
@@ -35,17 +31,15 @@ import static lombok.AccessLevel.PRIVATE;
 @Service
 @Slf4j
 public class SchoolService {
-  private final ExecutorService singleThreadExecutor = Executors.newSingleThreadExecutor();
   private static final String MINCODE_ATTRIBUTE = "mincode";
   private static final int DIST_NO_LENGTH = 3;
-  private Map<Mincode, SchoolEntity> mincodeSchoolEntityMap;
   private final ReadWriteLock minCodeSchoolMapLock = new ReentrantReadWriteLock();
-
   /**
    * The School repository.
    */
   @Getter(PRIVATE)
   private final SchoolRepository schoolRepository;
+  private Map<Mincode, SchoolEntity> mincodeSchoolEntityMap;
 
   /**
    * Instantiates a new School service.
@@ -64,25 +58,17 @@ public class SchoolService {
    * @return the School entity if found.
    */
   public SchoolEntity retrieveSchoolByMincode(String mincode) {
-    try {
-      Optional<SchoolEntity> result = Optional.empty();
-      if (mincode.length() > DIST_NO_LENGTH) {
-        var distNo = mincode.substring(0, DIST_NO_LENGTH);
-        var schoolNumber = mincode.substring(DIST_NO_LENGTH);
-        final Callable<SchoolEntity> callableSchool = () -> this.mincodeSchoolEntityMap.get(Mincode.builder().distNo(distNo).schlNo(schoolNumber).build());
-        result = Optional.ofNullable(singleThreadExecutor.submit(callableSchool).get());
-      }
-
-      if (result.isPresent()) {
-        return result.get();
-      } else {
-        throw new EntityNotFoundException(SchoolEntity.class, MINCODE_ATTRIBUTE, mincode);
-      }
-    } catch (final ExecutionException | InterruptedException exception) {
-      Thread.currentThread().interrupt();
-      throw new SchoolAPIRuntimeException(exception.getMessage());
+    Optional<SchoolEntity> result = Optional.empty();
+    if (mincode.length() > DIST_NO_LENGTH) {
+      val distNo = mincode.substring(0, DIST_NO_LENGTH);
+      val schoolNumber = mincode.substring(DIST_NO_LENGTH);
+      result = Optional.ofNullable(this.mincodeSchoolEntityMap.get(Mincode.builder().distNo(distNo).schlNo(schoolNumber).build()));
     }
-
+    if (result.isPresent()) {
+      return result.get();
+    } else {
+      throw new EntityNotFoundException(SchoolEntity.class, MINCODE_ATTRIBUTE, mincode);
+    }
   }
 
   /**
@@ -92,8 +78,7 @@ public class SchoolService {
    */
   public List<SchoolEntity> retrieveAllSchools() {
     try {
-      final Callable<List<SchoolEntity>> listCallableSchool = () -> new ArrayList<>(this.mincodeSchoolEntityMap.values());
-      return singleThreadExecutor.submit(listCallableSchool).get();
+      return new CopyOnWriteArrayList<>(this.mincodeSchoolEntityMap.values());
     } catch (final Exception exception) {
       throw new SchoolAPIRuntimeException(exception.getMessage());
     }
@@ -104,11 +89,11 @@ public class SchoolService {
    */
   @PostConstruct
   public void init() {
-    singleThreadExecutor.execute(this::setSchoolData);
+    this.setSchoolData();
   }
 
   private void setSchoolData() {
-    Lock writeLock = minCodeSchoolMapLock.writeLock();
+    val writeLock = minCodeSchoolMapLock.writeLock();
     try {
       writeLock.lock();
       this.mincodeSchoolEntityMap = schoolRepository.findAll().stream().collect(Collectors.toConcurrentMap(SchoolEntity::getMincode, Function.identity()));
