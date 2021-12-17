6b2e8945993f205278e1e731ded792a9a8cf5367
Mingwei <mingwei.y@gmail.com>
2021-12-17 15:57:56 -0800

    Ignore unknown json fields of FedProvSchoolCode

--- api/src/main/java/ca/bc/gov/educ/api/school/struct/v1/FedProvSchoolCode.java
+++ api/src/main/java/ca/bc/gov/educ/api/school/struct/v1/FedProvSchoolCode.java
@@ -1,13 +1,19 @@
 package ca.bc.gov.educ.api.school.struct.v1;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class FedProvSchoolCode {
   @Pattern(regexp = "NOM_SCHL", message = "Only support nom school")
   String key;
