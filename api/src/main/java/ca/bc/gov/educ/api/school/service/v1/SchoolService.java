865c196611efebefd8edb76cc9f3ee6740165503
harshit-kohli <76568232+harshit-kohli@users.noreply.github.com>
2022-03-02 12:58:02 -0800

    Update SchoolService.java

--- api/src/main/java/ca/bc/gov/educ/api/school/service/v1/SchoolService.java
+++ api/src/main/java/ca/bc/gov/educ/api/school/service/v1/SchoolService.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
